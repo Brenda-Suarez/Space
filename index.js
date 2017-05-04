ccd0ed9d6dee0d5fe870112860fefe937bc5d851
Brenda Suarez <bsuarez314@gmail.com>
2017-05-04 11:12:18 -0600

    added images and minor changes
    added all of my planet images and changed some css and added some
    images to the react file.

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 // JavaScript Document
+import React, { Component } from 'react';
+import { AppRegistry, View, Image } from 'react-native';
 const { createElement } = React
 const { render } = ReactDOM
+
 
 
 const title = createElement(
@@ -13,3 +16,19 @@ render(
 	title,
 	document.getElementById('react-container')
 )
+
+
+class DisplayAnImage extends Component {
+  render() {
+    return (
+      <View>
+        <Image
+          source={require('./images/sun.png')}
+        />
+        <Image
+          source={require('./images/mercury.png')}
+        />
+      </View>
+    );
+  }
+}
