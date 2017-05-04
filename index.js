1166666e54d2d3aeed2dc33cda0b7807d15ce844
Brenda Suarez <bsuarez314@gmail.com>
2017-05-04 11:32:17 -0600

    changed react code
    changed the code

--- index.js
+++ index.js
@@ -1,36 +1,15 @@
 // JavaScript Document
 import React, { Component } from 'react';
 import { AppRegistry, View, Image } from 'react-native';
-const { createElement } = React
 const { render } = ReactDOM
 
 
-
-const title = createElement(
-	'h1',
-	{id: 'title', className: 'header'},
-	'THE SOLAR SYSTEM'
-)
-
 render(
-	title,
+	<h1 id ='title'
+		className='header'
+	THE SOLAR SYSTEM
+	</h1>,
 	document.getElementById('react-container')
 )
 
 
-class DisplayPlanets extends Component {
-  render() {
-    return (
-      <View>
-        <Image
-          source={require('./images/sun.png')}
-        />
-        <Image
-          source={require('./images/mercury.png')}
-        />
-      </View>
-    );
-  }
-}
-
-AppRegistry.registerComponent('DisplayPlanets', () => DisplayPlanets);
