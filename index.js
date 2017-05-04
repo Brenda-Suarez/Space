e8dfd447034bce80625876c68b46ab859e95713f
Brenda Suarez <bsuarez314@gmail.com>
2017-05-04 11:35:02 -0600

    I don't know what this is
    Im trying to revert to an older commit

--- index.js
+++ index.js
@@ -4,6 +4,15 @@ import { AppRegistry, View, Image } from 'react-native';
 const { render } = ReactDOM
 
 
+<<<<<<< HEAD
+=======
+const title = createElement(
+	'h1',
+	{id: 'title', className: 'header'},
+	'The Solar System'
+)
+
+>>>>>>> parent of 1ac109b...  changed title and added more css
 render(
 	<h1 id ='title'
 		className='header'
